818004a5279d1afab4d7b7fa5af707bd5c9bea8a
suchenguo <guozhenduo1@163.com>
2020-07-04 19:54:10 +0800

    dict1.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='password',
+	version="0.1.0",
+	keywords=['password','generate'],
+	author='suchenguo',
+	author_email='guozhenduo1@163.com',
+	description="密码生成器",
+	packages=find_packages()
+)
